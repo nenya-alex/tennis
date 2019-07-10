62f6a1b98fd0aa27e5ce3a1e36027c17af82eb41
Alex <alexni@ukr.net>
2019-07-10 21:33:04 +0300

    changed entities

--- src/main/java/ua/tennis/service/ProbabilityService.java
+++ src/main/java/ua/tennis/service/ProbabilityService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import ua.tennis.domain.Game;
 import ua.tennis.domain.Sett;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +12,9 @@ public class ProbabilityService {
 
     public List<Sett> getGameProbabilities(double homeOdds, double awayOdds, byte numberOfSets){
         List<Sett> result = new ArrayList<>();
-        Game game = new Game().probabilityAway(BigDecimal.ONE).probabilityHome(BigDecimal.TEN);
-        Sett set = new Sett().addGame(game).awayScore(1).homeScore(2).probabilityAway(BigDecimal.valueOf(11))
-            .probabilityHome(BigDecimal.valueOf(13));
+        Game game = new Game().awayProbability(1.0).homeProbability(0.7);
+        Sett set = new Sett().addGame(game).awayScore(1).homeScore(2).awayProbability(0.5)
+            .homeProbability(0.33);
         result.add(set);
         return result;
     }
